da8ca6b769b89c99cb70be114510ea6cae5dc3ad
shrekTheLegend69 <shrekthesaviour69@gmail.com>
2023-04-07 08:39:28 +0200

    Make plan for function game

--- game.js
+++ game.js
@@ -24,41 +24,54 @@ function randomThree() {
 
 // Make a function that plays a single round of Rock, Paper and Scissors with two parameters - playerSelection, computerSelection
 function singleRound(playerSelection, computerSelection) {
-// Compare playerSelection and computerSelection
-// Make playerSelection and computerSelection lower case to avoid problems when comparing
-playerSelection = playerSelection.toLowerCase();
-computerSelection = computerSelection.toLowerCase();
-// Scissors beats paper
-if ((playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors")) {
-    if (playerSelection === "scissors") {
-        return "Winner, winner, chicken dinner!";
+    // Compare playerSelection and computerSelection
+    // Make playerSelection and computerSelection lower case to avoid problems when comparing
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    // Scissors beats paper
+    if ((playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors")) {
+        if (playerSelection === "scissors") {
+            return "Winner, winner, chicken dinner!";
+        }
+        else {
+            return "You lose, try again!";
+        }
     }
+    // Paper beats Rock
+    else if ((playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "rock" && computerSelection === "paper")) {
+        if (playerSelection === "paper") {
+            return "Winner, winner, chicken dinner!";
+        }
+        else {
+            return "You lose, try again!";
+        }
+    }
+    // Rock beats scissors
+    else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
+        if (playerSelection === "rock") {
+            return "Winner, winner, chicken dinner!";
+        }
+        else {
+            return "You lose, try again!";
+        }
+    }
+    // If same choice, return draw message
     else {
-        return "You lose, try again!";
+        return "Draw, play again!";
+    }
+    // If win, return win message
+    // If lose, return lose message
+}
+
+//Make a function game, that will play 5 round game of rock, paper and scissors and keeps the score and report winner or loser at the end
+function game()  {
+    // Play single round game 5 times and keep a score inside
+    for (i = 0; i < 5; i++) {
+        // Get players input
+        // Call singleRound function
+        // Write score every round
+        // Write who won in the end
     }
 }
-// Paper beats Rock
-else if ((playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "rock" && computerSelection === "paper")) {
-    if (playerSelection === "paper") {
-        return "Winner, winner, chicken dinner!";
-    }
-    else {
-        return "You lose, try again!";
-    }
-}
-// Rock beats scissors
-else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
-    if (playerSelection === "rock") {
-        return "Winner, winner, chicken dinner!";
-    }
-    else {
-        return "You lose, try again!";
-    }
-}
-// If same choice, return draw message
-else {
-    return "Draw, play again!";
-}
-// If win, return win message
-// If lose, return lose message
-}
+
+
