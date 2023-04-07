e791ad1f1e90f45858c5ae22e2baf1f72f792d8f
shrekTheLegend69 <shrekthesaviour69@gmail.com>
2023-04-07 08:17:11 +0200

    Make a plan for code, make functions getComputerChoice, randomThree, start work on singleRound function

--- game.js
+++ game.js
@@ -21,3 +21,25 @@ function randomThree() {
     const randomThree = Math.floor(Math.random() * 3) + 1;
     return randomThree;
 }
+
+// Make a function that plays a single round of Rock, Paper and Scissors with two parameters - playerSelection, computerSelection
+function singleRound(playerSelection, computerSelection) {
+// Compare playerSelection and computerSelection
+// Make playerSelection and computerSelection lower case to avoid problems when comparing
+playerSelection = playerSelection.toLowerCase();
+computerSelection = computerSelection.toLowerCase();
+// Scissors beats paper
+if ((playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors")) {
+    if (playerSelection === "scissors") {
+        return "Winner, winner, chicken dinner!";
+    }
+    else {
+        return "You lose, try again!";
+    }
+}
+// Paper beats Rock
+// Rock beats scissors
+// If same choice, return draw message
+// If win, return win message
+// If lose, return lose message
+}
